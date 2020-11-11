9d8cf0b7d1e3cb7d365a273f09f33932887b24d8
csjohn1 <csjohn@shaw.ca>
2020-11-11 19:45:29 +0000

    committing exercise 4 and 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+sum = Store.sum("annual_revenue")
+puts sum
+avg = Store.average("annual_revenue")
+puts avg
+whale = Store.where(annual_revenue: 1000000..100000000)
+puts whale.count
