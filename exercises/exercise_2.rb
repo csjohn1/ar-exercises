22c13858a053400ca94ff42cc807cdcffb41c68b
csjohn1 <csjohn@shaw.ca>
2020-11-11 19:13:38 +0000

    committing exercise 1 and 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,8 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+puts @store1.to_s
+@store1.name = "Burntown"
+@store1.save
